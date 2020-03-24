f63994d7839257000671cabc54387ded6e928e5a
Rounak Agarwal <rounakr.ag73@gmail.com>
2020-03-24 14:52:47 +0530

    updating stripe integration and updating our favicon and title

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+
+import { selectDirectorySections } from '../../redux/directory/directory.selectors';
 
 import MenuItem from '../menu-item/menu-item.component';
 
 import './directory.styles.scss';
 
-const Directory = ({sections}) => (
+const Directory = ({ sections }) => (
     <div className='directory-menu'>
         {sections.map(({ id, ...otherSectionProps }) => (
             <MenuItem key={id} {...otherSectionProps} />
@@ -13,4 +16,8 @@ const Directory = ({sections}) => (
     </div>
 );
 
-export default Directory;
+const mapStateToProps = createStructuredSelector({
+    sections: selectDirectorySections
+});
+
+export default connect(mapStateToProps)(Directory);
